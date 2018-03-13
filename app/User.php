0bad51be7c9ff41dc220190ac87c31fd21f3aa6c
Raymond <Raymondng154@gmail.com>
2018-03-13 04:59:14 -0400

    Factory and Seed

--- app/User.php
+++ app/User.php
@@ -29,6 +29,6 @@ class User extends Authenticatable
     
     public function question()
     {
-        return $this->hasMany(Question:class);
+        return $this->hasMany(Question::class);
     }
 }
